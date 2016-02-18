57836b022a4d2d50e69f6d6109555aa24b40ee35
plectrum0224 <plectrum@outlook.com>
2016-02-18 22:53:43 +0800

    Master OOP page 62
    20160218 night

--- PycharmProjects/OOP/special_method/initMethod.py
+++ PycharmProjects/OOP/special_method/initMethod.py
@@ -59,19 +59,21 @@ def card(rank, suit):
 
     elif 2 <= rank < 11:
         return NumberCard(rank, suit)
-#****************************************************************#
+    # ****************************************************************#
     elif 11 <= rank < 14:
         # name = {11: 'J', 12: 'Q', 13: 'K'}[rank]
         return FaceCard(rank, suit)
 
     else:
         raise Exception("Rank out of range")
-#****************************************************************#
+    # ****************************************************************#
     # try other....
     # else:
     #     name = {11: 'J', 12: 'Q', 13: 'K'}[rank]
     #     return FaceCard(name, suit)
-#****************************************************************#
+
+
+# ****************************************************************#
 # use elif exchange the mapping method
 #     elif rank == 11:
 #         return FaceCard('J', suit)
@@ -82,16 +84,41 @@ def card(rank, suit):
 #     else:
 #         raise Exception("Rank out of range")
 # =====================================================================================================================#
-# main function
+# wrapping a collection class
 # =====================================================================================================================#
+# class Deck(object):
+#     def __init__(self):
+#         self._card = []
+#         for rank in range(1, 14):
+#             for suit in (Club, Diamond, Heart, Spade):
+#                 self._card.append(card(rank, suit))
+#         random.shuffle(self._card)
+#
+#     def pop(self):
+#         return self._card.pop()
+# =====================================================================================================================#
+# extending a collection class
+# =====================================================================================================================#
+# class Deck(list):
+#     def __init__(self):
+#         super(Deck, self).__init__(card(r+1, s) for r in range(13) for s in (Club, Diamond, Heart, Spade))
+#         random.shuffle(self)
+
+class Deck(list):
+    def __init__(self, decks=6):
+        super(Deck, self).__init__()
+        for i in range(decks):
+            self.extend(card(r+1, s) for r in range(13) for s in (Club, Diamond, Heart, Spade))
+            # random.shuffle(self)
+            # burn = random.randint(1, 52)
+            # for i in range(burn):self.pop()
+
+
 if __name__ == '__main__':
     Club, Diamond, Heart, Spade = Suit('Club', '♣'), Suit('Diamond', '♦'), Suit('Heart', '♥'), Suit('Spade', '♠')
 
-    deck = []
-    for rank in range(1, 14):
-        for suit in (Club, Diamond, Heart, Spade):
-            deck.append(card(rank, suit))
-    random.shuffle(deck)
-    hand = [deck.pop(), deck.pop()]
-    for item in hand:
-        print("rank: {0}\tsuit: {1}".format(item.rank, item.suit.symbol))
+    d = Deck()
+    print(len(d))
+    # hand = [d.pop(), d.pop()]
+    # for item in hand:
+    #     print("rank: {0}\tsuit: {1}".format(item.rank, item.suit.symbol))

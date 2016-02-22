c69753f561cbf6ceb17bca71292658fc14bdbf48
plectrum0224 <plectrum@outlook.com>
2016-02-22 22:58:33 +0800

    策略模式修改一些代码
    20160222晚

--- PycharmProjects/OOP/DesignPattern/strategyPattern.py
+++ PycharmProjects/OOP/DesignPattern/strategyPattern.py
@@ -60,7 +60,7 @@
 # m1 = ModelDuck()
 # m1.performFly()
 # m1.performQuack()
-#==============================================================#武器策略
+#==============================================================#四个武器装备
 class SwordBehavior(object):
     def __init__(self):
         self.behaviorCode = "SWORD"
@@ -72,14 +72,23 @@ class KnifeBehavior(object):
     def __init__(self):
         self.behaviorCode = "KNIFE"
 
+    def __call__(self):
+        return "use the knife"
+
 class BowAndArrowBehavior(object):
     def __init__(self):
         self.behaviorCode = "BOWANDARROW"
 
+    def __call__(self):
+        return "use the bow and arrow"
+
 class AxeBehavior(object):
     def __init__(self):
         self.behaviorCode = "AXE"
-#==============================================================#人物策略
+
+    def __call__(self):
+        return "use the axe"
+#==============================================================#三个人物设定
 class KingFigure(object):
     def __init__(self):
         self.figureCode = "KING"
@@ -91,13 +100,43 @@ class QueenFigure(object):
     def __init__(self):
         self.figureCode = "QUEEN"
 
+    def __call__(self):
+        return "I am a queen"
+
 class TrollFigure(object):
     def __init__(self):
         self.figureCode = "TROLL"
 
+    def __call__(self):
+        return "I am a troll"
+
 class KnightFigure(object):
     def __init__(self):
         self.figureCode = "KNIGHT"
+
+    def __call__(self):
+        return "I am a knight"
+#==============================================================#三种攻击策略
+class attack1Method(object):
+    def __init__(self):
+        self.attackCode = "ATTACK1"
+
+    def __call__(self):
+        return "with attack1"
+
+class attack2Method(object):
+    def __init__(self):
+        self.attackCode = "ATTACK2"
+
+    def __call__(self):
+        return "with attack2"
+
+class attack3Method(object):
+    def __init__(self):
+        self.attackCode = "ATTACK3"
+
+    def __call__(self):
+        return "with attack3"
 #==============================================================#
 class CharacterCreator(object):
     def __init__(self):
@@ -109,20 +148,27 @@ class CharacterCreator(object):
                               QueenFigure(),
                               TrollFigure(),
                               KnightFigure()]
+        self.__attackImpls = [attack1Method(),
+                              attack2Method(),
+                              attack3Method]
 
-    def __call__(self, weapon, character):
+    def __call__(self, weapon, character, attack):
         for wimpl in self.__weaponImpls:
-            for fimpl in self.__figureImpls:
-                if wimpl.behaviorCode == weapon and fimpl.figureCode == character:
-                    return fimpl() + " " +wimpl()
-            else:
-                return None
+            if wimpl.behaviorCode == weapon:
+                for fimpl in self.__figureImpls:
+                    if fimpl.figureCode == character:
+                        for aimpl in self.__attackImpls:
+                            if aimpl.attackCode == attack:
+                                return fimpl() + " " +wimpl() + " " + aimpl()
 
 
 def main():
     chracterCreator = CharacterCreator()
-    king = chracterCreator("SWORD", "KING")
+    king = chracterCreator("SWORD", "KING", "ATTACK1")
+    queen = chracterCreator("KNIFE", "QUEEN", "ATTACK2")
     print(king)
+    print(queen)
+
 
 
 if __name__ == '__main__':
